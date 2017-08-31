9dc882fc29712065f25497358456d91759c6acb1
jhantz <jhantz@CHQCOOP1453>
2017-08-31 13:10:54 -0400

    Add project files.

---
+++ Imbored/DataObjects/Question.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Prism.Mvvm;
+
+namespace Imbored.DataObjects
+{
+    public class Question : BindableBase
+    {
+        private string _query;
+        private List<string> _answers;
+        private int _correctAnswer;
+
+        [XmlElement("Query")]
+        public string Query
+        {
+            get { return _query; }
+            set { SetProperty(ref _query, value); }
+        }
+        
+        [XmlElement("Answer")]
+        public List<string> Answers
+        {
+            get { return _answers; }
+            set { SetProperty(ref _answers, value); }
+        }
+        
+        [XmlElement("CorrectAnswer")]
+        public int CorrectAnswer
+        {
+            get { return _correctAnswer; }
+            set { SetProperty(ref _correctAnswer, value); }
+        }
+    }
+}
---
+++ Imbored/ViewModels/QuizViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Imbored.DataObjects;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Serialization;
+using Microsoft.Win32;
+using Prism.Commands;
+
+namespace Imbored.ViewModels
+{
+    public class QuizViewModel : BindableBase
+    {
+        private Question _currentQuestion;
+
+        public Question CurrentQuestion
+        {
+            get { return _currentQuestion; }
+            set
+            {
+                SetProperty(ref _currentQuestion, value);
+            }
+        }
+
+        private IEnumerable<Question> _questionList;
+
+        public IEnumerable<Question> QuestionList
+        {
+            get { return _questionList; }
+            set { _questionList = value; }
+        }
+
+        public QuizViewModel()
+        {
+            OpenFileCommand = new DelegateCommand(OpenFile);
+            SelectAnswerCommand = new DelegateCommand<int?>(SelectAnswer);
+        }
+
+        private void SelectAnswer(int? answer)
+        {
+            MessageBox.Show(answer == CurrentQuestion.CorrectAnswer ? "Correct" : "Incorrect");
+        }
+
+        private void OpenFile()
+        {
+            var deSerializer = new XmlSerializer(typeof(Question));
+            var dialog = new OpenFileDialog();
+            var showDialog = dialog.ShowDialog() ?? false;
+            if (showDialog)
+            {
+                using (Stream reader = dialog.OpenFile())
+                {
+                    object obj = deSerializer.Deserialize(reader);
+                    Question q = obj as Question;
+
+                    if (q != null)
+                    {
+                        CurrentQuestion = q;
+                    }
+                    else
+                    {
+                        MessageBox.Show("The file is corrupt!", "Corrupt File", MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+
+        public ICommand OpenFileCommand { get; set; }
+
+        public ICommand SelectAnswerCommand { get; set; }
+    }
+}
