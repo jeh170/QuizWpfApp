f4e4dbe8dd548bbc14b1d5fb0c8e1c88db39603c
jeh170 <jeh170@pitt.edu>
2017-08-31 14:28:21 -0400

    Added NewQuestionViewModel with variables

---
+++ Imbored/ViewModels/NewQuestionViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Common;
+using Prism.Mvvm;
+
+namespace Imbored.ViewModels
+{
+    public class NewQuestionViewModel : BindableBase
+    {
+        private IList<ObservableObject<string>> _answers;
+        private string _query;
+
+        private int _correctAnswer;
+
+        public int CorrectAnswer
+        {
+            get { return _correctAnswer; }
+            set { SetProperty(ref _correctAnswer, value); }
+        }
+        
+        public string Query
+        {
+            get { return _query; }
+            set { SetProperty(ref _query, value); }
+        } 
+
+        public IList<ObservableObject<string>> Answers
+        {
+            get { return _answers; }
+            set {SetProperty(ref _answers, value); }
+        }
+    }
+}
