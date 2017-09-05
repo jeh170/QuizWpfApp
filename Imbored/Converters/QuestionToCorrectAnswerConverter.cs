12bd8e02c6d6d38975b2bb100f45c0ca40828431
jeh170 <jeh170@pitt.edu>
2017-09-05 10:54:51 -0400

    Data Template for question list, added converter to implement

---
+++ Imbored/Converters/QuestionToCorrectAnswerConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Imbored.DataObjects;
+
+namespace Imbored.Converters
+{
+    public class QuestionToCorrectAnswerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var q = value as Question;
+
+            return q?.Answers[q.CorrectAnswer];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
